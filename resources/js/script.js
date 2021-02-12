15afd2c7fce9734ab4acc36d9a4c1f8cb55de996
PERTAMINA\mk.yudha.kuswandi1 <mk.yudha.kuswandi1@mitrakerja.pertamina.com>
2021-02-12 23:12:39 +0700

    Pelengkap

--- resources/js/script.js
+++ resources/js/script.js
@@ -114,15 +114,64 @@ $('.js--nav-icon').click(function() {
 
 
 
-var d = new Date(new Date().getTime() + 200 * 120 * 120 * 2000);
-simplyCountdown('.countdown', {
-    year: d.getFullYear(),
-    month: d.getMonth() + 1,
-    day: d.getDate()
+//var d = new Date(new Date().getTime() + 200 * 120 * 120 * 2000);
+// simplyCountdown('.countdown', {
+//     year: 2021,
+//     month: 2,
+//     day: 20
+// });
+
+// var d = new Date(),
+// countUpDate = new Date();
+// d.setDate(d.getDate() + 450);
+
+// default example
+
+  
+
+simplyCountdown('.simply-countdown-one', {
+    year: 2021,
+    month: 2,
+    day: 20,
+    enableUtc: true
 });
-$('#simply-countdown-losange').simplyCountdown({
-    year: d.getFullYear(),
-    month: d.getMonth() + 1,
-    day: d.getDate(),
-    enableUtc: false
+
+var mymap = L.map('mapid').setView([-6.25, 106.87], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoieXVkaGFrIiwiYSI6ImNrbDJnY2YxcDE0eGYybmxiNGRlcWF4bzQifQ.gmXI526ChIJ0mIYeX2PMqQ'
+}).addTo(mymap);
+
+var marker = L.marker([-6.253752827177549, 106.8704594978605]).addTo(mymap);
+
+var popup = L.popup()
+    .setLatLng([-6.253752827177549, 106.8704594978605])
+    .setContent("Disini acaranya")
+    .openOn(mymap);
+
+    //-6.253752827177549, 106.8704594978605
+
+
+const button = document.querySelector("#button");
+const icon = document.querySelector("#button > i");
+const audio = document.querySelector("audio");
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-off');
+    
+  } else {
+    audio.pause();
+    icon.classList.remove('fa-volume-off');
+    icon.classList.add('fa-volume-up');
+  }
+  button.classList.add("fade");
 });
